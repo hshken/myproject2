cc918579957ee4882a94c1c1331de8ab2ad2bc7b
hshken <hshken@livemail.tw>
2019-02-26 10:15:33 +0800

    first commit

---
+++ src/com/java2/student/GraduateStudent.java
@@ -0,0 +1,7 @@
+package com.java2.student;
+
+public class GraduateStudent extends Student{
+	public GraduateStudent() {
+		super("µL¦W¤ó",0);
+	}
+}
--- src/com/java2/student/Student.java
+++ src/com/java2/student/Student.java
@@ -1,5 +1,14 @@
 package com.java2.student;
 
 public class Student {
-
+	String name;
+	int English;
+	public Student(String name, int english) {
+		this.name=name;
+		this.English=english;
+		}
+		public void print() {
+			System.out.println(name + "\t" + English);
+		
+	}
 }
